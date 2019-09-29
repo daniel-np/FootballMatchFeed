75f74c1c58341d8a07515aaa3d7942d2135bc06d
Daniel <d.pettersen@gmail.com>
2019-09-29 15:56:51 +0100

    Remade the model classes to reduce the amount of stored data to only what was needed currently in the app

--- app/src/main/java/com/example/incrowdapp/data/commentary/Commentary.kt
+++ app/src/main/java/com/example/incrowdapp/data/commentary/Commentary.kt
@@ -3,16 +3,45 @@ package com.example.incrowdapp.data.commentary
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-class Commentary {
+data class Commentary(
 
-    @SerializedName("status")
-    @Expose
-    var status: String? = null
     @SerializedName("data")
     @Expose
     var data: Data? = null
-    @SerializedName("metadata")
-    @Expose
-    var metadata: Metadata? = null
+)
 
-}
+data class Data(
+    @SerializedName("homeTeamName")
+    @Expose
+    var homeTeamName: String? = null,
+    @SerializedName("homeScore")
+    @Expose
+    var homeScore: Int? = null,
+    @SerializedName("awayTeamName")
+    @Expose
+    var awayTeamName: String? = null,
+    @SerializedName("awayScore")
+    @Expose
+    var awayScore: Int? = null,
+    @SerializedName("competition")
+    @Expose
+    var competition: String? = null,
+    @SerializedName("commentaryEntries")
+    @Expose
+    var commentaryEntries: List<CommentaryEntry>? = null
+)
+
+data class CommentaryEntry(
+    @SerializedName("type")
+    @Expose
+    var type: String? = null,
+    @SerializedName("comment")
+    @Expose
+    var comment: String? = null,
+    @SerializedName("period")
+    @Expose
+    var period: Int? = null,
+    @SerializedName("time")
+    @Expose
+    var time: String? = null
+)
